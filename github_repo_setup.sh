643396ff3bc62a12e3a617f4fa88873b92fb94ea
Wes Lloyd-laptop <wlloyd@acm.org>
2017-01-07 17:07:12 -0800

    updates and add testdir

--- github_repo_setup.sh
+++ github_repo_setup.sh
@@ -1,4 +1,4 @@
-# git helper script
+# git helper script - describes common git operations
 
 # git config
 git config --global user.name "Wes Lloyd-laptop"
@@ -7,9 +7,16 @@ git config --global core.editor vim
 
 # first time repo setup - local
 git init
-git add <files!>
-# commit to local repo
+git add <files>
+# commit all changes to local repo
+git commit -a
+
+# otherwise you can incremently specify which files to update by adding them
+# individually with
+git add <files>
+# and then following this with
 git commit
+# this of course is slow, but very granular
 
 # initial push to github public cloud
 git remote add origin git@github.com:wjlloyd/test-repo-1.git
